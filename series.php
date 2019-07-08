7aaed32f572d1b3033077134efcddc5e497b3b3a
luisblafer <32635768+luisblafer@users.noreply.github.com>
2019-07-08 16:08:43 -0300

    cargo archivos

--- series.php
+++ series.php
@@ -2,6 +2,24 @@
 
 require("pdo.php");
 
+function consultarData($db, $idSerie)
+{
+    $query = $db->prepare('SELECT title FROM movies WHERE :idSerie');
+    $query->bindValue(':idSerie', $idSerie);
+    $query->execute();
+    $resultado = $consulta->fetchAll(PDO::FETCH_ASSOC);
+    return $resultado;
+}
+
+
+function consultarSerie($db, $idSerie)
+{
+    $consulta = 'SELECT title FROM movies WHERE $idSerie';
+    $serie = consultarData($db, $consulta);
+}
+
+
+
 function traerSeries(PDO $db) {
     $consulta = $db->prepare("SELECT * FROM series");
     //bind
@@ -15,9 +33,16 @@ $resultado = traerSeries($db);
 function mostrarSeries($resultado) {
     echo "<ul>";
     foreach ($resultado as $serie) {
-        echo "<li>" . "<a href="serie.php">" . $serie["title"] . "</a>" . "</li>";
+        echo "<li>" . "<a href='serie.php'>" . $serie["title"] . "</a>" . "</li>";
     }
     echo "</ul>";
 }
 
 mostrarSeries($resultado);
+
+
+
+function mostrarSerieId($db, $id)
+{
+    $
+}
